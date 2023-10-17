dc1b224de5445bfeff7e2540703dd9f9fedf4073
Pruthvi Shrikaanth <pruthvishrikaanth@gmail.com>
2023-10-17 07:46:25 +0100

    client gui

--- client_gui.py
+++ client_gui.py
@@ -13,6 +13,7 @@ print('connected')
 class GUI:
     def __init__(self):
         self.Window = Tk()
+        self.Window.withdraw()
         self.login = Toplevel()
         self.login.title = 'Login'
         self.login.resizable(width=False,height=False)
@@ -27,14 +28,15 @@ class GUI:
         self.go = Button(self.login,
 						text = "CONTINUE",
 						font = "Helvetica 14 bold",
-						command = lambda: self.goAhead(self.entryName.get()))
+						command = lambda: self.goAhead(self.nameEntry.get()))
         self.go.place(relx=0.4,rely=0.55)
+        self.login.mainloop()
         
     def goAhead(self,name):
         self.login.destroy()
-        self.name = name
         rcv = Thread(target=self.receive)
         rcv.start()
+        self.createChatWindow(name)
 
     def receive(self):
         while(True):
@@ -43,12 +45,62 @@ class GUI:
                 if(response.split(':')[0].strip()==self.name):
                     pass
                 else:
-                    print(response)
+                    # print(response)
+                    self.show_received_message(response)
             except:
-                print('Failed to fetch response')
+                # print('Failed to fetch response')
                 client.close()
                 break
-
+    def createChatWindow(self, name):
+        self.name = name
+        self.Window.deiconify()
+        self.Window.title('CHATROOM')
+        self.line = Label(self.Window,width=450,bg='#abb2b9')
+        self.line.place(relwidth=1,rely=0.07,relheight=0.01)
+        self.Window.resizable(width=False,height=False)
+        self.Window.configure(width=470,height=550,bg='lightgrey')
+        self.labelHead = Label(self.Window,text=('Welcome '+self.name),fg='blue',bg='white',font=('Helvetica',16))
+        self.labelHead.place(relwidth=1)
+        self.textCons = Text(self.Window,width=20,height=2,bg='#17202a',fg='#eaecee',font=('Helvetica',14),padx=5,pady=5)
+        self.textCons.place(relheight=0.745,relwidth=1,rely=0.08)
+        self.labelBottom = Label(self.Window,bg='#abb2b9',height=80)
+        self.labelBottom.place(relwidth=1,rely=0.825)
+        self.enterMessage = Entry(self.labelBottom,bg='#2c3e50',fg='#eaecee',font=('Helvetica',13))
+        self.enterMessage.place(relwidth=0.74,relheight=0.06,rely=0.008,relx=0.011)
+        self.enterMessage.focus()
+        self.sendButton = Button(self.labelBottom,text='Send',font=('Helvetica',13,'bold'),width=20,bg='#abb2b9',command=lambda:self.sendMessage(self.enterMessage.get()))
+        self.sendButton.place(relx=0.77,rely=0.008,relheight=0.06,relwidth=0.22)
+        self.textCons.config(cursor='arrow')
+        scrollBar = Scrollbar(self.textCons)
+        scrollBar.place(relheight=1,relx=0.974)
+        scrollBar.config(command=self.textCons.yview())
+        
+    def sendMessage(self,message):
+        self.textCons.config(state=DISABLED)
+        self.message = message
+        self.enterMessage.delete(0,END)
+        send = Thread(target=self.show_message(message))
+        send.start()
+    
+    def show_message(self,message):
+        self.textCons.config(state=NORMAL)
+        self.textCons.insert(END,message+'\n\n')
+        self.textCons.config(state=DISABLED)
+        self.textCons.see(END)
+        self.textCons.config(state=DISABLED)
+        self.write()
+    
+    def show_received_message(self,message):
+        self.textCons.config(state=NORMAL)
+        self.textCons.insert(END,message+'\n\n')
+        self.textCons.config(state=DISABLED)
+        self.textCons.see(END)
+    
+    def write(self):
+        self.textCons.config(state=DISABLED)
+        message = (f"{self.name}: {self.message}")
+        client.send(message.encode('utf-8'))
+        
 
 gui = GUI()
 
